b8669bda7696a6d9cf4e1102d15de9febff379ae
vstoms <vstoms@gmail.com>
2024-11-14 10:55:55 +0100

    Update constants.ts

--- lib/constants.ts
+++ lib/constants.ts
@@ -9,4 +9,4 @@ export const WASTE_ICONS: Record<WasteIconType, typeof Trash2> = {
   'wine': Wine,
 } as const
 
-export const API_URL = 'https://kalender.renovasjonsportal.no/api/address/cc56f50c-c1b0-424d-a228-75e88b1e2d27/details'
+export const API_URL = 'https://kalender.renovasjonsportal.no/api/address/CHANGE_THIS/details'
